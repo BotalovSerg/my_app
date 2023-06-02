4062ba38c7de2a756b6f0081093ae7451fbfd10c
Botalov Serg <89090168690@mail.ru>
2023-06-02 20:22:07 +0500

    add cache redis

--- service/services/models.py
+++ service/services/models.py
@@ -2,6 +2,8 @@ from typing import Any, Iterable, Optional
 from django.db import models
 from django.core.validators import MaxValueValidator
 from services.tasks import set_price, set_comment
+from django.db.models.signals import post_delete
+from services.receivers import delete_cache_total_sum
 
 from clients.models import Client
 
@@ -58,4 +60,14 @@ class Subscription(models.Model):
     price = models.PositiveIntegerField(default=0)
     comment = models.CharField(max_length=100, default='')
 
+    def save(self, *args, **kwargs):
+        creating = not bool(self.id)
+        result = super().save(*args, **kwargs)
 
+        if creating:
+            set_price.delay(self.id)
+
+        return result
+
+
+post_delete.connect(delete_cache_total_sum, sender=Subscription)
